a3a13e5d355b86e71e90692fda47b9ce554e907f
Jack Raley <jack.raley@hotmail.com>
2015-01-25 12:59:42 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix takes in a matrix object and returns a list of stored functions created within it. The list can be called
+## to set and retrieve data.
+## cacheSolve takes in a matrix object, determines if the inverse has been calculated and if not it calculates the inverse
+## and then returns it.
 
-## Write a short comment describing this function
-# The makeCacheMatrix function takes in a matrix 'x'. The object 'm' is set to NULL intially as a placeholder.
-# set is an internal function that assigns 'y' the matrix 'x' and sets the inverse variable 'm' to NULL. get is assigned
-# a function that returns 'x'. setinv is assigned a function call to the R function solve, it returns the matrix inverse
-# 'm'. getinv is assigned a function that returns 'm'. A list is created that can return all of the created information
-# from the makeCacheMatrix function call.
+# The makeCacheMatrix function takes in a matrix 'x'. The object 'm' is set to NULL intially as a placeholder within the
+# function. set is an internal function that assigns 'y' the matrix 'x' and sets the inverse variable 'm' to NULL. get is
+# assigned a function that returns 'x'. setinv is assigned a function call to the R function solve, it returns the matrix
+# inverse 'm'. getinv is assigned a function that returns 'm'. A list is created that can return all of the created
+# information from the makeCacheMatrix function call.
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+    m <- NULL                                   
     set <- function(y) {
         x <<- y
         m <<- NULL
@@ -22,14 +23,11 @@ makeCacheMatrix <- function(x = matrix()) {
          getinv = getinv)
 }
 
-
-## Write a short comment describing this function
 # cacheSolve takes in a matrix 'x'. It checks the environment list to see if the matrix inverse 'm' has already been solved 
 # for and if not then uses the R solve function to calculate it. Once the inverse 'm' is calculated it is then set and also
-# returned.
+# returned. If the inverse has already been calculated the function returns this stored inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
     m <- x$getinv()
     if(!is.null(m)) {
         message("getting cached data")
